f4922e170dfe05fdd173c76153876a52e0ca9dd2
Tan Jun Xiang <tjunxiang92@gmail.com>
2019-05-26 10:23:34 +0800

    Added Slide Question Answers

---
+++ exercises/src/com/tertiaryinfotech/day_1/SlideQuestions.java
@@ -0,0 +1,48 @@
+package com.tertiaryinfotech.day_1;
+
+
+public class SlideQuestions {
+
+//
+//    Write a Java program to convert seconds to hour, minute and seconds.
+//    Input seconds: 86399
+//            23:59:59
+//
+//    Write a Java program to calculate the modules of two numbers without using any inbuilt modulus operator
+//    Input the first number : 19
+//    Input the second number: 7
+//            5
+
+    public static void main(String[] args) {
+//        //    Write a Java program to reverse a string.
+//        //    Input a string: The quick brown fox
+//        //    Reverse string: xof nworb kciuq ehT
+//        String str = "The quick brown fox";
+//        for (int i = str.length() - 1; i >= 0; i--) {
+//            System.out.print(str.charAt(i));
+//        }
+//        System.out.println();
+
+//        // Write a Java program to convert seconds to hour, minute and seconds.
+//        // Input seconds: 86399
+//        // 23:59:59
+//        int seconds = 86319;
+//        System.out.printf("%d:%d:%d",
+//            (seconds / (60 * 60)),
+//            (seconds / 60) % 60,
+//            seconds % 60
+//        );
+
+//        //    Write a Java program to calculate the modules of two numbers without using any inbuilt modulus operator
+//        //    Input the first number : 19
+//        //    Input the second number: 7
+//        //    5
+//        int firstNum = 100;
+//        int secondNum = 3;
+//
+//        int temp = (firstNum / secondNum) * secondNum;
+//        System.out.println(temp);
+//        System.out.println("Modulo: " + (firstNum - temp));
+    }
+
+}
